245cea47f5f3f9efde23a78d5cb9e7d0fd6c4fcd
Kirat Bains <kiratbains09@gmail.com>
2024-03-11 15:03:12 -0700

    feat: early setup

---
+++ App.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
