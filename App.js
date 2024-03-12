6e9c5290988f7939fc606cd9959e898baab1cffa
Kirat Bains <kiratbains09@gmail.com>
2024-03-12 08:58:49 -0700

    feat: connecting/seeding db

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
+const knex = require('knex')(require('./knexfile')); 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require('cors');
+const router = require('./Routers/router-1');
+
 
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+// change the route name as per team agreement
+app.use('/test', router);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
--- migrations/20220428210334_create_inventories_table.js
+++ migrations/20220428210334_create_inventories_table.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
+module.exports.up = function (knex) {
   return knex.schema.createTable('inventories', (table) => {
     table.increments('id').primary();
     table
