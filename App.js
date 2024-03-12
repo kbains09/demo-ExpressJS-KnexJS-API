20e9d602b19b204bf4c3998ae73580ebad160732
Lakshmi <kandelakshmi13@gmail.com>
2024-03-12 15:17:25 -0700

    added routes file

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
