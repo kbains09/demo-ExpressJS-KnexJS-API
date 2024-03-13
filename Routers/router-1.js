72dd9309d905421fda017479bbb5a88e4ec12dad
Kirat Bains <kiratbains09@gmail.com>
2024-03-12 21:59:13 -0700

    feat: tix 16/18/19, put/post/delete ops warehouse

--- Routers/router-1.js
+++ Routers/router-1.js
@@ -1,12 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
-const knex = require('knex')(require('../knexfile'));
 const wareHouseController = require('../controller/wareHouseController')
 
 
 router.route('/warehouse').get(wareHouseController.index);
 router.route('/:id').get(wareHouseController.selectedId);
+router.route('/warehouse').post(wareHouseController.createWarehouse);
+router.route('/:id').put(wareHouseController.updateWarehouse);
+router.route('/:id').delete(wareHouseController.deleteWarehouse);
 
 
 
